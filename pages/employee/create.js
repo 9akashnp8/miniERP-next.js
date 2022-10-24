ef8c092507619d9cb242a7dec8b9926a4edbf188
Akash NP <akashnp1998@gmail.com>
2022-10-24 20:28:26 +0530

    Minor UI improvements

--- pages/employee/create.js
+++ pages/employee/create.js
@@ -1,12 +1,17 @@
 import Form from "../../components/CreateForm";
 
-import { Typography } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+import Head from "next/head";
 
 
 export default function CreateEmployeePage() {
     return (
         <div>
-            <Typography variant='h4' my={1}>Create Employee</Typography>
+            <Head>
+                <title>miniERP | Add Employee</title>
+            </Head>
+            <Typography variant='h4' my={1}>Add New Employee</Typography>
             <Form/>
         </div>
     )
--- pages/finance.js
+++ pages/finance.js
@@ -1,9 +1,15 @@
 import Table from '../components/Table';
+import Head from 'next/head';
+
+import Typography from '@mui/material/Typography';
 
 export default function Employees() {
     return (
         <div>
-            <h1>Finance</h1>
+            <Head>
+                <title>miniERP | Payments</title>
+            </Head>
+            <Typography variant='h4' my={1}>Finance</Typography>
             <Table
                 endpoint='payment'
                 columns={['Payment ID', 'Service', 'Payment Month', 'Payment Status', 'Amount', 'Invoice No.', 'Invoice Date', 'Payment Date']}
