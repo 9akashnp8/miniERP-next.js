862ff1a0120b4a567a5e794bf82093af513aa2fc
Akash NP <akashnp1998@gmail.com>
2023-01-06 08:08:37 +0530

    Add Login Page & Remove default Layout from it

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import Layout from '../components/Layout';
 
+
 function MyApp({ Component, pageProps }) {
+  
+  const getLayout = Component.getLayout || ((page) => {
   return (
     <Layout>
-      <Component {...pageProps} />
+      {page}
     </Layout>
-  )
+  )})
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
