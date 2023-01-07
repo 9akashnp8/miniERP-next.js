a420a3cc12965961f6b1da03e40e3d93fdde8f46
Akash NP <akashnp1998@gmail.com>
2023-01-07 22:38:53 +0530

    Configure _app file to use redux state

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,25 @@
 import Layout from '../components/Layout';
 
+import { Provider } from 'react-redux';
+import { useStore } from '../lib/store';
+
 function MyApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
 
   const getLayout = Component.getLayout || ((page) => {
-  return (
-    <Layout>
-      {page}
-    </Layout>
+    return (
+      <Provider store={store}>
+        <Layout>
+          {page}
+        </Layout>
+      </Provider>
   )})
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
